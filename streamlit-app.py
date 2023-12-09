e075f44d5a8ae527da051ac8fc2d7ccef2645735
Stormbreaker0811 <nimishgodbole409@gmail.com>
2023-12-09 13:22:59 +0530

    Starting Streamlit deployment

---
+++ streamlit-app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+st.title("Amazon Sales Prediction using Linear, Ridge and Lasso Regressions")
+st.write("Select the variable for prediction:")
